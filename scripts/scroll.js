75017df3e22d3c104419d0fcdb5df13bc57589b5
Nishant Patel <npatel@WTLDEV.NET>
2019-09-18 09:23:47 -0400

    fixed the scroll bug for Internet Explorer

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -5,3 +5,15 @@ $('.scroll_down').click(function() {
   return false; //doesnt show #href link in url
 });
 
+// Fix for "jumpy" background on IE11
+if(navigator.userAgent.match(/Trident\/7\./)) { // if IE
+	$('body').on("mousewheel", function () {
+		// remove default behavior
+		event.preventDefault();
+
+		//scroll without smoothing
+		var wheelDelta = event.wheelDelta;
+		var currentScrollPosition = window.pageYOffset;
+		window.scrollTo(0, currentScrollPosition - wheelDelta + 5);
+	});
+}
